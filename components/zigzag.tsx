421aa3d48b8664b45a74004082ee832c7c587e31
Hasala Abhilasha <87168716+hasalaonline@users.noreply.github.com>
2024-04-01 11:22:46 +0530

    Updated

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -284,35 +284,34 @@ export default function Zigzag() {
                 </div>
               </div>
             </div>
-
-            {/* 2nd item */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+  
+              {/* 3rd item */}
+              <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
                 data-aos="fade-up"
               >
                 <Image
                   className="max-w-full mx-auto md:max-w-none h-auto"
                   src={FeatImage05}
-                  width={540}
-                  height={405}
-                  alt="Features 02"
+                  width={300}
+                  height={300}
+                  alt="Features 01"
                 />
               </div>
               {/* Content */}
               <div
                 className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-                data-aos="fade-left"
+                data-aos="fade-right"
               >
-                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <div className="font-architects-daughter text-xl text-purple-600 mb-2">
                     More speed. Less spend
                   </div>
-                  <h3 className="h3 mb-3">Keep track of previously generated guides</h3>
+                  <h3 className="h3 mb-3">Informative Dashboard</h3>
                   <p className="text-xl text-gray-400 mb-4">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
+                   With this feature you
                     aliqua.
                   </p>
                   <ul className="text-lg text-gray-400 -mb-2">
@@ -350,6 +349,7 @@ export default function Zigzag() {
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
